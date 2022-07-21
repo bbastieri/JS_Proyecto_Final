eba84e50c01ee90a47c1b234f50fa569e7b4048b
b.bastieri <b.bastieri@live.com>
2022-07-20 23:11:02 -0300

    agrego constructor + suma total

--- JS/main.js
+++ JS/main.js
@@ -1,17 +1,35 @@
+/* ARRAY */
 const carritoCompras = []
 
+/* CONSTRUCTOR */
+class producto {
+    constructor (id, nombre, precio){
+        this.id = id;
+        this.nombre= nombre;
+        this.precio= precio;
+    }    
+}
+
+/* OBJETOS */
+const producto1 = new producto (1, "Top Olimpia", 3000)
+const producto2 = new producto (2, "Corpiño Heavy Metal", 2500)
+const producto3 = new producto (3, "Bombi Heavy Metal", 1700)
+const producto4 = new producto (4, "Portaligas Rebel", 2000)
+const producto5 = new producto (5, "Body Cherry Bomb", 3500)
+const producto6 = new producto (6, "Medias Ying Yang", 800)
+
+/* AGREGAR PRODUCTOS */
 function agregarAlCarrito (producto){
     carritoCompras.push(producto)
     console.log(carritoCompras)
 }
+/* PRODUCTOS AGREGADOS */
+agregarAlCarrito(producto2);
+agregarAlCarrito(producto3);
+agregarAlCarrito(producto5);
+agregarAlCarrito(producto6);
 
-agregarAlCarrito({id:1, nombre:"Top Olimpia", precio:3000});
-agregarAlCarrito({id:2, nombre:"Corpiño Heavy Metal", precio:2500})
-agregarAlCarrito({id:3, nombre:"Bombi Heavy Metal", precio:1700})
-agregarAlCarrito({id:4, nombre:"Portaligas Rebel", precio:2000})
-agregarAlCarrito({id:5, nombre:"Body Cherry Bomb", precio:3500})
-agregarAlCarrito({id:6, nombre:"Medias Ying Yang", precio:800})
-
+/* BORRAR PRODUCTOS */
 function borrarDelCarrito (numeroId){
     const productoAEliminar = carritoCompras.findIndex((producto) => producto.id === numeroId)
     if (productoAEliminar !== -1){
@@ -19,7 +37,12 @@ function borrarDelCarrito (numeroId){
     }
     console.log (carritoCompras)
 }
-
+/* PRODUCTOS ELIMINADOS */
 borrarDelCarrito(5);
+
+/* SUMAR PRECIOS */
+const totalCarrito = carritoCompras.reduce ((acumulador,item) => acumulador + item.precio, 0)
+console.log (totalCarrito)
+
 
 
