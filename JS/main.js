c5c5acc77b293e2282f9921ddd8036ff6036d762
b.bastieri <b.bastieri@live.com>
2022-08-23 21:34:32 -0300

    creo funciones

--- JS/main.js
+++ JS/main.js
@@ -2,51 +2,14 @@
 const productosEnCarrito =JSON.parse(localStorage.getItem("totalCarrito")) ?? [];
 document.getElementById("btnCarritoTotal").innerHTML = `${productosEnCarrito.length}`;
 
-/* STOCK */
-/*  const productosEnStock = [
-  {
-    id: 1,
-    nombre: "Top Olimpia",
-    precio: 3000,
-    imagen: "./../images/tops/top_olimpia.jpg",
-  },
-  {
-    id: 2,
-    nombre: "Corpiño Heavy Metal",
-    precio: 2500,
-    imagen: "./../images/corpiños/corpi_heavymetal.JPG",
-  },
-  {
-    id: 3,
-    nombre: "Bombi Heavy Metal",
-    precio: 1700,
-    imagen: "./../images/bombis/bombi_heavymetal.JPG",
-  },
-  {
-    id: 4,
-    nombre: "Portaligas Rebel",
-    precio: 2000,
-    imagen: "./../images/portaligas/portaligas_rebel.png",
-  },
-  {
-    id: 5,
-    nombre: "Body Cherry Bomb",
-    precio: 3500,
-    imagen: "./../images/bodys/body_cherry_bomb.png",
-  },
-  {
-    id: 6,
-    nombre: "Medias Ying Yang",
-    precio: 800,
-    imagen: "./../images/medias/media_yinyang_black.jpg",
-  },
-]; */
+/* FETCH .JSON */
 
 const cargarProductos = async () => {
   const response = await fetch ('../productos.json');
-  const responseFinal = await response.json()
-  console.log(responseFinal)
-  return responseFinal
+  const responseFinal = await response.json();
+  crearCards(responseFinal);
+  agregarProducto(responseFinal);
+  borrarProducto(responseFinal)
 };
 
 const productosEnStock = cargarProductos();
@@ -57,7 +20,8 @@ console.log(productosEnStock);
 /* CARDS */
 let cards = "";
 
-productosEnStock.forEach(({ id, nombre, precio, imagen }) => {
+function crearCards(productosEnStock){
+ productosEnStock.forEach(({ id, nombre, precio, imagen }) => {
   const idBoton = `add-cart${id}`;
   const idBotonDelete = `delete-cart${id}`;
   cards += `<div>
@@ -68,13 +32,16 @@ productosEnStock.forEach(({ id, nombre, precio, imagen }) => {
             <button id="${idBotonDelete}">Eliminar del carrito</button>
             <div>`;
 });
-
 document.getElementById("cardsProductos").innerHTML = cards;
+}
 
 /* AGREGAR AL CARRITO */
-productosEnStock.forEach((producto) => {
-  const idBoton = `add-cart${producto.id}`;
-  document.getElementById(idBoton).addEventListener("click", () => {
+
+
+function agregarProducto(productosEnStock) {
+  productosEnStock.forEach((producto) => {
+   const idBoton = `add-cart${producto.id}`;
+   document.getElementById(idBoton).addEventListener("click", () => {
     productosEnCarrito.push(producto);
     localStorage.setItem("totalCarrito", JSON.stringify(productosEnCarrito));
     const totalCarrito = productosEnCarrito.reduce(
@@ -94,10 +61,12 @@ productosEnStock.forEach((producto) => {
       },
     }).showToast();
   });
-});
+ });
+}
 
 /* BORRAR PRODUCTOS */
-productosEnStock.forEach((producto) => {
+function borrarProducto(productosEnStock) {
+  productosEnStock.forEach((producto) => {
   const idBotonDelete = `delete-cart${producto.id}`;
   document.getElementById(idBotonDelete).addEventListener("click", () => {
     console.log(productosEnCarrito + "carrito");
@@ -120,10 +89,12 @@ productosEnStock.forEach((producto) => {
         },
       }).showToast();
   });
-});
+ });
+}
 
 /* TOTAL CARRITO POPUP */
 let cardsPopUp = "";
+
 
 productosEnCarrito.forEach(({id, nombre, precio, imagen}) => {
     const idBotonDelete = `delete-cart${id}`;
@@ -133,8 +104,9 @@ productosEnCarrito.forEach(({id, nombre, precio, imagen}) => {
               <h4>$${precio}</h4>
               <div>`;             
   });
-
+  
 document.getElementById("productosAgregados").innerHTML = cardsPopUp;
+
 
 //* MEDIOS DE PAGO */
 /* const mediosDePago = [
